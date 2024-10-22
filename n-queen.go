c368f9ea041894d73d3d0ac344f3b09c7df08d64
USER <user@MacBook-Pro.local>
2024-10-23 04:41:38 +0900

    テストが通った

--- n-queen.go
+++ n-queen.go
@@ -1,8 +1,57 @@
 package nqueen
 
+import (
+	"fmt"
+	"log/slog"
+	"slices"
+)
+
 type nqueen struct{}
 
-func SolveNqueen(n int) (solutions [][]int, err error) {
-	solutions = make([][]int, 2)
-	return solutions, nil
+func SolveNqueen(n int) (solutions [][]int) {
+	solutions = SolveNqueenRecursively(n, []int{})
+	slog.Debug(fmt.Sprintf("total solutions: %v", solutions))
+	return solutions
+}
+
+func SolveNqueenRecursively(n int, board []int) (solutions [][]int) {
+	slog.Debug(fmt.Sprintf("call FindLocalNqueen(n:%d, board: %v)", n, board))
+	if len(board) == n {
+		slog.Debug(fmt.Sprintf("new solution: %v", board))
+		return [][]int{board}
+	}
+
+	for x := 0; x < n; x++ {
+		if slices.Contains(board, x) {
+			continue
+		}
+		if onCrossLine(board, x) {
+			continue
+		}
+		newBoard := make([]int, len(board), len(board)+1)
+		copy(newBoard, board)
+		newBoard = append(newBoard, x)
+		newSolutions := SolveNqueenRecursively(n, newBoard)
+		solutions = append(solutions, newSolutions...)
+	}
+
+	slog.Debug(fmt.Sprintf("return solutions: %v", solutions))
+	return solutions
+}
+
+func onCrossLine(board []int, queen int) bool {
+	queenRow := len(board)
+	for i, q := range board {
+		if AbsInt(queenRow-i) == AbsInt(queen-q) {
+			return true
+		}
+	}
+	return false
+}
+
+func AbsInt(x int) int {
+	if x >= 0 {
+		return x
+	}
+	return -x
 }
--- n-queen_test.go
+++ n-queen_test.go
@@ -1,21 +1,32 @@
 package nqueen_test
 
 import (
+	"log/slog"
+	"os"
 	"reflect"
 	"testing"
 
 	nqueen "github.com/inahym196/n-queen"
 )
 
+func init() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey && len(groups) == 0 {
+				return slog.Attr{}
+			}
+			return a
+		},
+	})))
+}
+
 func TestForQueen(t *testing.T) {
 	want := [][]int{
 		{1, 3, 0, 2},
 		{2, 0, 3, 1},
 	}
-	sols, err := nqueen.SolveNqueen(4)
-	if err != nil {
-		t.Fatalf("error: %v", err)
-	}
+	sols := nqueen.SolveNqueen(4)
 
 	if len(sols) != 2 {
 		t.Errorf("num of solution should be 2, but %d", len(sols))
